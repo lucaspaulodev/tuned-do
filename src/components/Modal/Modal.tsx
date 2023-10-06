d502657a6e78f2a0092a419b0e6f9fbbbf3830dd
Lucas Paulo <lucaspaulodev@gmail.com>
2023-10-06 00:12:37 -0300

    Adding types for Modal component.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,30 +1,38 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
 import './styles.scss'
+import { ReactNode } from 'react';
 
-export const Modal = ({trigger, title, description, children}) => {
+interface ModalDTO {
+    trigger: ReactNode;
+    title: string;
+    description: string;
+    children: ReactNode;
+}
+
+export const Modal = ({trigger, title, description, children}: ModalDTO) => {
     return (
         <Dialog.Root>
-                <Dialog.Trigger asChild>
-                    {trigger}
-                </Dialog.Trigger>
-                <Dialog.Portal>
-                <Dialog.Overlay className="DialogOverlay" />
-                <Dialog.Content className="DialogContent">
-                    <Dialog.Title className="DialogTitle">{title}</Dialog.Title>
-                    {description ? (
-                        <Dialog.Description className="DialogDescription">
-                            {description}
-                        </Dialog.Description>
-                    ) : null}
-                    {children}
-                    <Dialog.Close asChild>
-                    <button className="IconButton" aria-label="Close">
-                        <X size={20}/>
-                    </button>
-                    </Dialog.Close>
-                </Dialog.Content>
-                </Dialog.Portal>
-            </Dialog.Root>
+            <Dialog.Trigger asChild>
+                {trigger}
+            </Dialog.Trigger>
+            <Dialog.Portal>
+            <Dialog.Overlay className="DialogOverlay" />
+            <Dialog.Content className="DialogContent">
+                <Dialog.Title className="DialogTitle">{title}</Dialog.Title>
+                {description ? (
+                    <Dialog.Description className="DialogDescription">
+                        {description}
+                    </Dialog.Description>
+                ) : null}
+                {children}
+                <Dialog.Close asChild>
+                <button className="IconButton" aria-label="Close">
+                    <X size={20}/>
+                </button>
+                </Dialog.Close>
+            </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
     )
 }
