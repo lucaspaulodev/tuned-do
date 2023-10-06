91b9090118a42d21cf78315e3fcbc0786f5dd3b3
Lucas Paulo <lucapaulodev@gmail.com>
2023-10-05 22:18:57 -0300

    Creating CREATE_TODO and UPDATE_TODO mutations.

--- src/lib/mutations/mutations.ts
+++ src/lib/mutations/mutations.ts
@@ -1,5 +1,23 @@
 import { gql } from "@apollo/client";
 
+export const CREATE_TODO = gql`
+    mutation($createTodoInput: CreateTodoInput!) {
+        createTodo(createTodoInput: $createTodoInput) {
+            title
+            id
+            detail
+        }
+    }
+`
+export const UPDATE_TODO = gql`
+    mutation($updateTodoId: Int!, $updateTodoInput: UpdateTodoInput!) {
+        updateTodo(id: $updateTodoId, updateTodoInput: $updateTodoInput) {
+            title
+            id
+            detail
+        }
+    }
+`
 export const REMOVE_TODO = gql`
     mutation($removeTodoId: Int!) {
         removeTodo(id: $removeTodoId) {
