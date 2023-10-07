1ad82aa04a6d7627c7e5862a24dec9b32aacc633
Lucas Paulo <lucaspaulodev@gmail.com>
2023-10-06 22:03:10 -0300

    Setupping VITE to use .env variables.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: "/src",
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: "/src",
+      },
     },
-  },
+    define: {
+      VITE_API_PORT: JSON.stringify(env.VITE_API_PORT)
+    }
+  }
 })
